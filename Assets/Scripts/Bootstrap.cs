f9bc257c6688b18a687468e199c465c8c893da96
Laura Barton <laura@motionmathgames.com>
2019-11-06 00:15:33 -0800

    Add timer to Bootstrap so that it automatically loads the Main scene

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bootstrap : MonoBehaviour
+{
+    [SerializeField] private float _delayTime = 1f;
+
+    float _startTime;
+
+    void Start()
+    {
+        _startTime = Time.timeSinceLevelLoad + _delayTime;
+    }
+
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad > _startTime)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
