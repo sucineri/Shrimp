6714f16a8cea802487d0dc6c018a0396602e61bf
Laura Barton <laura@motionmathgames.com>
2019-11-05 23:17:04 -0800

    Play exploding shrimp particles when button is clicked

--- Assets/Scripts/ShrimpCounter.cs
+++ Assets/Scripts/ShrimpCounter.cs
@@ -7,12 +7,15 @@ public class ShrimpCounter : MonoBehaviour
     public static event Action<int> CountChanged;
 
     [SerializeField] private TMP_Text _countText = null;
+    [SerializeField] private GameObject _clickParticles = null;
 
     private int _killCount;
 
     private void Start()
     {
         _killCount = 0;
+        _clickParticles.SetActive(false);
+
         UpdateCount();
     }
 
@@ -25,6 +28,9 @@ public class ShrimpCounter : MonoBehaviour
     {
         _killCount++;
         UpdateCount();
+
+        _clickParticles.SetActive(false);
+        _clickParticles.SetActive(true);
 
         CountChanged?.Invoke(_killCount);
     }
