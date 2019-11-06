52d7a4360b09f54c14566838885bda979cd47e6f
Laura Barton <laura@motionmathgames.com>
2019-11-05 22:23:46 -0800

    Create basic incrementation on button click

---
+++ Assets/Scripts/ShrimpCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class ShrimpCounter : MonoBehaviour
+{
+    public static event Action<int> CountChanged;
+
+    [SerializeField] private TMP_Text _countText = null;
+
+    private int _killCount;
+
+    private void Start()
+    {
+        _killCount = 0;
+        UpdateCount();
+    }
+
+    private void UpdateCount()
+    {
+        _countText.text = _killCount.ToString();
+    }
+
+    public void IncrementClicked()
+    {
+        _killCount++;
+        UpdateCount();
+
+        CountChanged?.Invoke(_killCount);
+    }
+}
